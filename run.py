5774a0b90760b180251fab948473d0d270dc9930
t0bes1 <131770004+t0bes1@users.noreply.github.com>
2023-09-07 10:57:04 +0000

    Add goals input code

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,6 +22,13 @@ form = SHEET.worksheet("form")
 team = SHEET.worksheet("team")
 
 
+def get_player_list():
+    players = appear.row_values(1)
+    remove_blank = players.pop(0)
+
+    return players
+
+
 def get_game_data():
     print("Please enter which game has been played")
     print("Example: 6\n")
@@ -31,66 +39,89 @@ def get_game_data():
     return game_data
 
 
-def get_player_list():
-    players = appearances.row_values(1)
-    remove_blank = players.pop(0)
-
-    return players
-
-
 def get_appearance_data(players, game_data):
-    """
-    MOVE VALIDATION & CELL UPDATE INSIDE FOR LOOP
-    """
+    """ """
 
     print("Please enter which player feature in the last match")
     print("Example: y\n")
 
     for x in players:
-        player_appearance = input(f"Did {x} play in the game (y/n):")
+        while True:
+            player_appearance = input(f"Did {x} play in the game (y/n):")
+            print("Validating data ...")
+
+            if validate_appearance_data(player_appearance):
+                print("Data is valid!")
+                break
         if player_appearance == "y":
             played = player_appearance.replace("y", "1")
         elif player_appearance == "n":
             played = player_appearance.replace("n", "0")
-        else:
-            print("Data is not valid")
-            break
         print("Adding to the tracker ...")
         appear.update_cell(game_data, int(players.index(x) + 2), int(played))
 
-    # get data from ss and do validation
 
-
-def validate_data():
+def validate_appearance_data(player_appearance):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
+    try:
+        if len(player_appearance) != 1:
+            raise ValueError(
+                f"Exactly 1 value required, you provided {len(player_appearance)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
     return True
 
 
-def update_worksheet(cell, data, worksheet):
+def get_goals_data(players, game_data):
+    """ """
+
+    print("Please enter which player feature in the last match")
+    print("Example: y\n")
+
+    for x in players:
+        while True:
+            player_goals = input(f"How many goals did {x} score?:")
+            print("Validating data ...")
+
+            if validate_goals_data(player_goals):
+                print("Data is valid!")
+                break
+
+        print("Adding to the tracker ...")
+        goals.update_cell(game_data, int(players.index(x) + 2), int(player_goals))
+
+
+def validate_goals_data(player_goals):
     """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
-    print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet(worksheet)
-    worksheet_to_update.update(cell)
-    print(f"{worksheet} worksheet updated successfully\n")
+    try:
+        if len(player_goals) != 1:
+            raise ValueError(
+                f"Exactly 1 value required, you provided {len(player_goals)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 def main():
     players = get_player_list()
-    print(players)
     game_data = get_game_data()
-    print(game_data)
-    appearance_data = get_appearance_data(players, game_data)
-    appearances.update_cell(game_data, 2, appearance_data)
-
-    # update_worksheet(cell, appearance_data, "appearances")
-    # print(appearance_data)
+    get_appearance_data(players, game_data)
+    os.system("cls")
+    get_goals_data(players, game_data)
 
 
 print("Welcome to Everett Rovers Football stats reporting")
