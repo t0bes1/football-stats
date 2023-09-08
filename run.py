a5635fe774213cd7d5761f280a8f86c03b249e25
t0bes1 <131770004+t0bes1@users.noreply.github.com>
2023-09-08 14:24:38 +0000

    Refactor total goals code

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("football-statistics-u9y")
 
 appear = SHEET.worksheet("appearances")
-goals = SHEET.worksheet("goals")
+gls = SHEET.worksheet("goals")
 form = SHEET.worksheet("form")
 team = SHEET.worksheet("team")
 
@@ -25,16 +25,16 @@ team = SHEET.worksheet("team")
 def get_player_list():
     players = appear.row_values(1)
     remove_blank = players.pop(0)
-    print(players)
 
     return players
 
 
 def get_game_data():
+    print("\nThe first thing we need is data")
     print("Please enter which game has been played")
     print("Example: 6\n")
 
-    raw_game_data = input("Enter your data here: ")
+    raw_game_data = input("Enter the Game Number here: ")
     game_data = int(raw_game_data) + 1
 
     return game_data
@@ -43,35 +43,34 @@ def get_game_data():
 def get_appearance_data(players, game_data):
     """ """
 
-    print("Please enter which player feature in the last match")
-    print("Example: y\n")
+    print("Please enter which player featured in the last match")
+    print("Example: 'y' or 'n'")
 
     for x in players:
         while True:
-            player_appearance = input(f"Did {x} play in the game (y/n):")
-            print("Validating data ...")
+            player_app = input(f"\nDid {x} play in the game (y/n):")
 
-            if validate_appearance_data(player_appearance):
-                print("Data is valid!")
+            if validate_appearance_data(player_app):
+                print("  Data is valid!")
                 break
-        if player_appearance == "y":
-            played = player_appearance.replace("y", "1")
-        elif player_appearance == "n":
-            played = player_appearance.replace("n", "0")
-        print("Adding to the tracker ...")
+        if player_app == "y":
+            played = player_app.replace("y", "1")
+        elif player_app == "n":
+            played = player_app.replace("n", "0")
+        print("  Adding to the tracker ...")
         appear.update_cell(game_data, int(players.index(x) + 2), int(played))
 
 
-def validate_appearance_data(player_appearance):
+def validate_appearance_data(player_app):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
     try:
-        if len(player_appearance) != 1:
+        if len(player_app) != 1:
             raise ValueError(
-                f"Exactly 1 value required, you provided {len(player_appearance)}"
+                f"Exactly 1 value required, you provided {len(player_app)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
@@ -81,34 +80,36 @@ def validate_appearance_data(player_appearance):
 
 
 def get_goals_data(players, game_data):
-    """ """
+    """
+    The
+    """
 
-    print("Please enter which player feature in the last match")
-    print("Example: y\n")
+    print("\nPlease enter the goals scored by the player in this match")
+    print("This should be a number and could be 0")
+    print("Example: 1 or 2\n")
 
     for x in players:
         while True:
-            player_goals = input(f"How many goals did {x} score?:")
-            print("Validating data ...")
+            player_gls = input(f"How many goals did {x} score?:")
 
-            if validate_goals_data(player_goals):
-                print("Data is valid!")
+            if validate_goals_data(player_gls):
+                print("  Data is valid!")
                 break
 
-        print("Adding to the tracker ...")
-        goals.update_cell(game_data, int(players.index(x) + 2), int(player_goals))
+        print("  Adding to the tracker ...")
+        gls.update_cell(game_data, int(players.index(x) + 2), int(player_gls))
 
 
-def validate_goals_data(player_goals):
+def validate_goals_data(player_gls):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
     try:
-        if len(player_goals) != 1:
+        if len(player_gls) != 1:
             raise ValueError(
-                f"Exactly 1 value required, you provided {len(player_goals)}"
+                f"Exactly 1 value required, you provided {len(player_gls)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
@@ -117,76 +118,83 @@ def validate_goals_data(player_goals):
     return True
 
 
-def calculate_total_appearances(game_data, players):
-    all_appearances = appear.get_all_values()
+def calculate_total_app(game_data, players):
+    """
+    Accesses full appearance data from the spreadsheet
+    This is manipulated into totals for each player
+    """
+    all_app = appear.get_all_values()
+    remove_names = all_app.pop(0)
 
-    all_appear_col = []
+    all_app_array = []
     colnum = len(players) + 1
+    rangenum = game_data - 1
 
-    for z in range(colnum):
-        new_all_appearances = []
-        for x in all_appearances:
+    for z in range(1, colnum):
+        all_app_list = []
+        for x in all_app:
             i = x[z]
-            new_all_appearances.append(i)
+            all_app_list.append(i)
 
-        all_appear_col.append(new_all_appearances)
+        all_app_array.append(all_app_list)
 
-    aac2 = []
+    total_app_string = []
 
-    for i in all_appear_col:
-        j = i[1:game_data]
-        aac2.append(j)
+    for i in all_app_array:
+        j = i[0:rangenum]
+        total_app_string.append(j)
 
-    aac3 = aac2[1:colnum]
+    total_app = []
 
-    total_appear = []
-
-    for i in aac3:
+    for i in total_app_string:
         q = sum([eval(k) for k in i])
-        total_appear.append(q)
+        total_app.append(q)
 
-    print(total_appear)
-
-    return total_appear
+    return total_app
 
 
-def calculate_total_goals(game_data, players):
-    all_goals = goals.get_all_values()
+def calculate_total_gls(game_data, players):
+    """
+    Accesses full goals data from the spreadsheet
+    This is manipulated into totals for each player
+    """
+    all_gls = gls.get_all_values()
+    remove_names = all_gls.pop(0)
 
-    all_goals_col = []
+    all_gls_array = []
     colnum = len(players) + 1
+    rangenum = game_data - 1
 
-    for z in range(colnum):
-        new_all_goals = []
-        for x in all_goals:
+    for z in range(1, colnum):
+        all_gls_list = []
+        for x in all_gls:
             i = x[z]
-            new_all_goals.append(i)
+            all_gls_list.append(i)
 
-        all_goals_col.append(new_all_goals)
+        all_gls_array.append(all_gls_list)
 
-    agc2 = []
+    total_gls_string = []
 
-    for i in all_goals_col:
-        j = i[1:game_data]
-        agc2.append(j)
+    for i in all_gls_array:
+        j = i[0:rangenum]
+        total_gls_string.append(j)
 
-    agc3 = agc2[1:colnum]
+    total_gls = []
 
-    total_goals = []
-
-    for i in agc3:
+    for i in total_gls_string:
         q = sum([eval(k) for k in i])
-        total_goals.append(q)
+        total_gls.append(q)
 
-    print(total_goals)
-
-    return total_goals
+    return total_gls
 
 
 def calculate_form(total_goals, total_appear, players):
-    form = [a / b for a, b in zip(total_goals, total_appear)]
+    """
+    Uses goals and appearance data to calcuate a "form" metric for each player
+    The highest value is returned, representing the best current player
+    """
 
-    print(form)
+    form = [a / b for a, b in zip(total_goals, total_appear)]
 
     ranking1 = form.index(max(form))
 
@@ -200,13 +208,17 @@ def main():
     game_data = get_game_data()
     get_appearance_data(players, game_data)
     get_goals_data(players, game_data)
-    total_appear = calculate_total_appearances(game_data, players)
-    total_goals = calculate_total_goals(game_data, players)
-    no1_rank = calculate_form(total_goals, total_appear, players)
-    print("Thank you for inputting the latest macth scores.")
-    print(f"The top ranked player is {no1_rank}, please select him for the next match")
+    total_app = calculate_total_app(game_data, players)
+    total_gls = calculate_total_gls(game_data, players)
+    no1_rank = calculate_form(total_gls, total_app, players)
+    print("Thank you for inputting the latest match scores.")
+    print(f"The top ranked player is {no1_rank}")
+    print("Please select him for the next match")
 
 
+print("\n")
 print("Welcome to Everett Rovers Football stats reporting")
-print("Input the latest match figures")
+print("This program helps you input match figures")
+print("After inputting the figures, it will show you the best form player")
+print("Please include this player in the next team selection")
 main()
